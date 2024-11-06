a5da9eb479c7d6c3138ff8de189c30270c43663d
boogiebump <sabda.alam2308@gmail.com>
2024-11-07 01:37:26 +0700

    Create float.js

---
+++ template/nexus/float.js
@@ -0,0 +1,21 @@
+document.write('<div id="floatads" style="width: 100%;margin: auto;text-align: center;position: fixed;bottom: -270px;left: 50%;transform: translateX(-50%);z-index: 9999;overflow: hidden;">');
+document.write('<div style="text-align:center;display:block;max-width:750px;height:300px;overflow:hidden;margin:auto">');
+
+
+//Paste Iklan Disini
+
+atOptions = {
+		'key' : '30a3f40cafd7139c9a7921d764adf606',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/30a3f40cafd7139c9a7921d764adf606/invoke.js"></scr' + 'ipt>');
+
+
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
