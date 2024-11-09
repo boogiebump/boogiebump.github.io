ec9aad33b130dffdf342d31892efa83f01577605
boogiebump <sabda.alam2308@gmail.com>
2024-11-09 11:09:27 +0700

    Update float.js

--- template/nexus/float.js
+++ template/nexus/float.js
@@ -4,16 +4,6 @@ document.write('<div style="text-align:center;display:block;max-width:750px;heig
 
 //Paste Iklan Disini
 
-atOptions = {
-		'key' : '30a3f40cafd7139c9a7921d764adf606',
-		'format' : 'iframe',
-		'height' : 50,
-		'width' : 320,
-		'params' : {}
-	};
-document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/30a3f40cafd7139c9a7921d764adf606/invoke.js"></scr' + 'ipt>');
-
-
 
 // Batas Iklan
 
