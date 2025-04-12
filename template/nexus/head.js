755a31220cd0a9db5f358180eefbef5020c552ef
boogiebump <sabda.alam2308@gmail.com>
2025-04-13 00:40:24 +0700

    Update head.js

--- template/nexus/head.js
+++ template/nexus/head.js
@@ -1,5 +1,5 @@
 //Histats
-document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4853726&101"/>');
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4943554&101"/>');
 
 ///// Statcounter /////
 var sc_project=13049188; 
