5dec995124610ac618820717960d25db33611819
boogiebump <sabda.alam2308@gmail.com>
2026-04-02 09:56:14 +0700

    Update and rename stats.js to st.js

--- bl/st.js
+++ bl/st.js
@@ -1,5 +1,5 @@
 //Histats
-document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4919244&101"/>');
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4981581&101"/>');
 
 ///// Statcounter /////
 var sc_project=13049188; 
